37a5bc49f2df2ebd4b99d296bb39dbc14b7eb5ff
EuanAbM <158186430+EuanAbM@users.noreply.github.com>
2024-05-03 00:44:58 +0100

    Enhanced search.php to output results in a table format for AJAX updates.

--- _includes/search.php
+++ _includes/search.php
@@ -7,7 +7,17 @@ $searchTerm = mysqli_real_escape_string($conn, $searchTerm);
 $sql = "SELECT * FROM student WHERE studentid LIKE '%$searchTerm%' OR firstname LIKE '%$searchTerm%' OR lastname LIKE '%$searchTerm%' OR postcode LIKE '%$searchTerm%'";
 $result = mysqli_query($conn, $sql);
 
-while ($row = mysqli_fetch_array($result)) {
-    // Output each row in a format that matches your table
+$output = '<table class="table table-striped">';
+while($row = mysqli_fetch_assoc($result)) {
+    $output .= '<tr>
+                    <td>' . $row['studentid'] . '</td>
+                    <td>' . $row['firstname'] . '</td>
+                    <td>' . $row['lastname'] . '</td>
+                    <td>' . $row['dob'] . '</td>
+                    <td>' . $row['postcode'] . '</td>
+                </tr>';
 }
+$output .= '</table>';
+echo $output;
+
 ?>
