40ee432a245154a99afed8562b440f9c63427e80
EuanAbM <158186430+EuanAbM@users.noreply.github.com>
2024-05-03 01:32:25 +0100

    Updated edit_student.php to match the new design layout

--- _includes/edit_student.php
+++ _includes/edit_student.php
@@ -62,125 +62,75 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Student Profile</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- Font Awesome for icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <style>
-        .profile-image {
-            width: 150px;
-            height: 150px;
-            border-radius: 50%;
-            object-fit: cover;
-            margin: 0 auto;
-            display: block;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <div class="container">
+        <!-- Back to Students Button -->
+        <div class="d-flex justify-content-end mt-2 mb-3">
+            <a href="students.php" class="btn btn-info">Back to Students</a>
+        </div>
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Edit Student Profile</div>
-                <div class="card-body">
-                    <!-- Student Information Form -->
-                    <form method="post" action="" enctype="multipart/form-data">
-                        <!-- Profile Image -->
-                        <div class="form-group text-center">
-                            <label for="image">Profile Image</label><br>
-                            <?php if (!empty($student['image'])) : ?>
-                                <img src="<?php echo $student['image']; ?>" alt="Student Image" class="profile-image mb-3">
-                            <?php else : ?>
-                                <img src="placeholder.jpg" alt="Student Image" class="profile-image mb-3">
-                            <?php endif; ?>
-                            <input type="file" class="form-control-file" id="image" name="image">
-                        </div>
-                        
-                        <!-- Student Personal Details -->
-                        <div class="form-group">
-                            <label for="firstname">First Name</label>
-                            <input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo $student['firstname']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="lastname">Last Name</label>
-                            <input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo $student['lastname']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="dob">Date of Birth</label>
-                            <input type="date" class="form-control" id="dob" name="dob" value="<?php echo $student['dob']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="house">House</label>
-                            <input type="text" class="form-control" id="house" name="house" value="<?php echo $student['house']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="town">Town</label>
-                            <input type="text" class="form-control" id="town" name="town" value="<?php echo $student['town']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="county">County</label>
-                            <input type="text" class="form-control" id="county" name="county" value="<?php echo $student['county']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="country">Country</label>
-                            <input type="text" class="form-control" id="country" name="country" value="<?php echo $student['country']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="postcode">Postcode</label>
-                            <input type="text" class="form-control" id="postcode" name="postcode" value="<?php echo $student['postcode']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Update Password</label>
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Enter new password or leave blank for no change">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Update Profile</button>
-                    </form>
-                    
-                    <!-- Back Button -->
-                    <a href="students.php" class="btn btn-secondary mt-3"><i class="fas fa-arrow-left"></i> Back to Students</a>
-                    
-                    <!-- Emergency Contacts Preview -->
-                    <hr>
-                    <h5>Emergency Contacts</h5>
+        <!-- Student ID and Course Name -->
+        <div class="card mb-3">
+            <div class="card-header bg-primary text-white">Edit (Student ID) Profile</div>
+            <div class="card-body">
+                <form method="POST" action="" enctype="multipart/form-data">
                     <div class="row">
-                        <?php foreach ($emergencyContacts as $contact) : ?>
-                            <div class="col-md-4 mb-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h6 class="card-title"><?php echo $contact['relation']; ?></h6>
-                                        <p class="card-text"><?php echo $contact['firstname'] . ' ' . $contact['lastname']; ?></p>
-                                        <p class="card-text"><?php echo $contact['phone']; ?></p>
-                                    </div>
-                                </div>
+                        <!-- Student Photo -->
+                        <div class="col-md-3">
+                            <div class="mb-3">
+                                <label for="image" class="form-label">Student Photo</label>
+                                <input type="file" class="form-control" id="image" name="image" onchange="previewFile()">
+                                <img id="preview" src="<?php echo $student['image']; ?>" alt="Student Photo" class="img-thumbnail mt-2">
                             </div>
-                        <?php endforeach; ?>
+                        </div>
+                        <!-- Student Details -->
+                        <div class="col-md-9">
+                            <div class="mb-3">
+                                <label for="firstname" class="form-label">First Name</label>
+                                <input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo $student['firstname']; ?>" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="lastname" class="form-label">Last Name</label>
+                                <input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo $student['lastname']; ?>" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" value="<?php echo $student['email']; ?>" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="phone" class="form-label">Phone</label>
+                                <input type="text" class="form-control" id="phone" name="phone" value="<?php echo $student['phone']; ?>" required>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    <div class="d-flex justify-content-end">
+                        <button type="submit" class="btn btn-success">Update Profile</button>
+                    </div>
+                </form>
             </div>
         </div>
+        <!-- Other sections to be updated in similar fashion -->
     </div>
-</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        function previewFile() {
+            var preview = document.getElementById('preview');
+            var file    = document.querySelector('input[type=file]').files[0];
+            var reader  = new FileReader();
 
-<!-- Bootstrap JS and jQuery (place before </body>) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script>
-    // JavaScript for image preview
-    $(document).ready(function() {
-        $('#image').change(function() {
-            var input = this;
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    $(input).closest('.form-group').find('.profile-image').attr('src', e.target.result);
-                };
-                reader.readAsDataURL(input.files[0]);
+            reader.onloadend = function () {
+                preview.src = reader.result;
+            };
+
+            if (file) {
+                reader.readAsDataURL(file);
+            } else {
+                preview.src = "";
             }
-        });
-    });
-</script>
-
+        }
+    </script>
 </body>
 </html>
